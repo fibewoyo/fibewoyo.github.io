3ee2f88770f1534197a875345f5efe62e7e8aa4a
fibewoyo <94056341+fibewoyo@users.noreply.github.com>
2021-11-10 20:14:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function() {
+    //show popup modal
+    var myModal = new bootstrap.Modal(document.getElementById('modalPopUp'));
+    myModal.show();
+    // jika klik ok maka : 
+    $('#btnOk').click(function() {
+        var audio = new Audio("lagu.mp3");
+        audio.play();
+        //sembunyikan popup
+        myModal.hide();
+    });
+    // jika klik close maka :
+    $('#btnClose').click(function() {
+        alert("Yahh masa gak mau :(")
+            //kembali di redirect ke halaman yang sama
+        
+    });
+    $('#closeX').click(function() {
+        alert("Yahh masa gak mau :(")
+            //kembali di redirect ke halaman yang sama
+        
+    });
+
+
+});
